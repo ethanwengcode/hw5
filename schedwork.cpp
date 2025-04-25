871c26dcd62ff7b14ce2947b8ea38e2412d81965
Ethan Weng <ethanwen@usc.edu>
2025-04-25 04:41:33 +0000

    done

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,12 +19,17 @@ using namespace std;
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
-
 // Add prototypes for any helper functions here
-
+bool scheduleHelper(
+  const AvailabilityMatrix& avail,
+  const size_t dailyNeed,
+  const size_t maxShifts,
+  DailySchedule& sched,
+  size_t day,
+  vector<int>& numShifts
+);
 
 // Add your implementation of schedule() and other helper functions here
-
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -38,8 +43,65 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    size_t days = avail.size(); // number of days
+    size_t workers = avail[0].size(); // number of workers 
 
-
-
+    sched.resize(days); // resize for "days" number of rows
+    vector<int> numShifts(workers, 0); // tracks the number of shifts for each worker
+    bool success = scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, numShifts);
+    return success;
 }
 
+bool scheduleHelper(
+  const AvailabilityMatrix& avail,
+  const size_t dailyNeed,
+  const size_t maxShifts,
+  DailySchedule& sched,
+  size_t day,
+  vector<int>& numShifts
+) {
+  if(day == sched.size()) { // finished scheduling for every day; done
+    return true; 
+  }
+
+  if(sched[day].size() == dailyNeed) { // current day is filled with shifts, go to next day 
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, day +1, numShifts);
+  }
+
+  // itereate through each worker
+  for(size_t w = 0; w < avail[0].size(); w++) {
+    // is the current worker available ?
+    bool workerAvail = avail[day][w];
+    if( !workerAvail ) {
+      continue;  // go to next worker
+    }
+    // has the current worker reached their shift limit?
+    int workerNumShift = numShifts[w];
+    if(maxShifts <= workerNumShift) {
+      continue;
+    }
+    // has the current worker already been schedule for this day?
+    bool workerScheduled = false;
+    for(vector<Worker_T>::iterator it = sched[day].begin(); it!= sched[day].end(); ++it) {
+      if(*it == w){
+        workerScheduled = true;
+        break;
+      }
+    }
+    if(workerScheduled) {
+      continue;
+    }
+
+    sched[day].push_back(w); // schedule current worker for this day
+    numShifts[w]++; // increment curr worker's shifts
+
+    // able to schedule for all other days 
+    if(scheduleHelper(avail, dailyNeed, maxShifts, sched, day, numShifts)) {
+      return true;
+    }
+
+    sched[day].pop_back(); // backtrack
+    numShifts[w]--; // decrement curr worker's shifts
+  }
+  return false; // indicate that we weren't able to schedule for all days
+}
--- wordle.cpp
+++ wordle.cpp
@@ -11,9 +11,15 @@
 #include "dict-eng.h"
 using namespace std;
 
-
 // Add prototypes of helper functions here
-
+void wordleHelper(
+    string temp,
+    const string& in,
+    string floating,
+    const set<string>& dict,
+    set<string>& res,
+    int index
+);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +28,57 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    set<string> res;
+    string temp = in; 
+    wordleHelper(temp, in, floating, dict, res, 0);
+    return res;
 }
 
 // Define any helper functions here
+void wordleHelper(
+    string temp,
+    const string& in,
+    string floating,
+    const set<string>& dict,
+    set<string>& res,
+    int index
+) {
+    // full word is created 
+    if(index == in.size()) {
+        // check if it's a true English-language word and uses the floating characters
+        if(dict.find(temp) != dict.end() && floating.empty()) {
+            res.insert(temp);  // store in results
+        }
+        return;
+    }
+
+    char c = in[index]; // get current character
+    if(c == '-') { // blank
+        // count number of blank locations
+        int numBlank = 0;
+        for(size_t i = index; i < in.size(); i++) {
+            if( in[i] == '-') {
+                numBlank++;
+            }
+        }
+
+        // try all characters
+        for(char j = 'a'; j <= 'z'; j++) {
+            string floatingChars = floating; // stores floating chars
+
+            if( floating.find(j) != string::npos) { // floating character
+                floatingChars.erase(floating.find(j), 1); // remove the single character; index is where the char was found at
+                temp[index] = j; // replace '-' with the floating char
+                wordleHelper(temp, in, floatingChars, dict, res, index +1);
+            }
+            else if( floating.size() < numBlank ) { // have more blanks than floating characters
+                temp[index] = j;
+                wordleHelper(temp, in, floatingChars, dict, res, index +1);
+            }
+        }
+    }
+    else { // not blank
+        temp[index] = c;
+        wordleHelper(temp, in, floating, dict, res, index+1); // go to next character
+    }
+} 
